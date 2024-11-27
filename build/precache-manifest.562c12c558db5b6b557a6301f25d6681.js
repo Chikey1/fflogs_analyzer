b144b1c5c57570f4e49ceadb9f29f07253c90e87
Sara Qi <sara.qi@skydio.com>
2024-11-26 23:25:58 -0800

    deploy react app

---
+++ build/precache-manifest.562c12c558db5b6b557a6301f25d6681.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4652a7bb7965a6e238336cf85c05604",
+    "url": "/index.html"
+  },
+  {
+    "revision": "142a807593913b5add14",
+    "url": "/static/css/main.76a72078.chunk.css"
+  },
+  {
+    "revision": "93e999e7af063c0a8229",
+    "url": "/static/js/2.ade98681.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.ade98681.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "142a807593913b5add14",
+    "url": "/static/js/main.9b3b2d9b.chunk.js"
+  },
+  {
+    "revision": "9664f680d79c459df2a6",
+    "url": "/static/js/runtime-main.09078e83.js"
+  }
+]);
